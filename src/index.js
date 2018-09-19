a419ef29dcb43bb122e53e99fc669dda7dbb86dd
Sches <naste4ka11.11@gmail.com>
2018-09-19 20:55:40 +0300

    Solve task

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
 class Sorter {
   constructor() {
     // your implementation
+      this.array = [];
+      this.leng = 0;
+      this.compareFunction = function(a,b){
+        return a-b;
+      }
   }
 
   add(element) {
     // your implementation
+    this.array.push(element);
+    this.leng = this.leng + 1;
   }
 
   at(index) {
     // your implementation
+    return this.array[index];
   }
 
   get length() {
     // your implementation
+    return this.leng;
   }
 
   toArray() {
     // your implementation
+    return this.array;
   }
 
   sort(indices) {
     // your implementation
+    this.indices = indices;
+    this.buff = [];
+    let i;
+    this.indices.sort();
+    for(i = 0; i < indices.length; i++)
+    {
+      this.buff[i] = this.array[this.indices[i]]
+    }
+    this.buff.sort(this.compareFunction);
+    for(i = 0; i < this.buff.length; i++)
+    {
+      this.array[this.indices[i]] = this.buff[i];
+    }
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
